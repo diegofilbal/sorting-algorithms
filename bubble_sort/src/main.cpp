7fdfb74f059b88b930c8d59931869b28d0fbd2e8
Diego Filgueiras <diegofilbal@ufrn.edu.br>
2020-10-08 18:57:34 -0300

    change how to evaluate the length of the list

--- bubble_sort/src/main.cpp
+++ bubble_sort/src/main.cpp
@@ -5,9 +5,7 @@ int main (){
     vector <int> list {78, 16, 47, 5, 54, 0, 99, 54, 25, 63, 2, 30, 6};
 
     // Evaluates the length of the list
-    size_t tam = list.end() - list.begin();
-
-    cout << "Tamanho: " << tam << endl;
+    size_t tam = list.size();
 
     // Prints the unsorted list
     for (size_t i = 0; i < tam; i++){
