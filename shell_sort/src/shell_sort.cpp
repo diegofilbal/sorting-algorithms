777c6806a2c617c471473a6001bb1c294cb13ded
Diego Filgueiras <diegofilbal@ufrn.edu.br>
2020-10-10 20:25:27 -0300

    add comments to shell_sort.cpp

--- shell_sort/src/shell_sort.cpp
+++ shell_sort/src/shell_sort.cpp
@@ -3,17 +3,21 @@
 
 void shell(vector <int> &list){
 
-    // Holds the length of the list
+    //  Holds the length of the list
     size_t tam = list.size();
 
+    //  Sets the gap for comparison
     size_t gap = tam;
 
     for (size_t i = 0; i < tam; i++){
 
+        //  Cuts the gap by half
         gap /= 2;
         
+        //  Compares the elements according to the current gap
         for (size_t j = 0; j < tam-gap; j++){
 
+            //  Sorts the sublist elements
             if (list.at(j) > list.at(j+gap)){
                 int aux = list.at(j);
                 list.at(j) = list.at(j+gap);
