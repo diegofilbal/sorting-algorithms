5385dd724932918acd97875c63732eab07410c50
Diego Filgueiras <diegofilbal@ufrn.edu.br>
2020-10-11 00:00:06 -0300

    add merge sort algorithm

---
+++ merge_sort/src/main.cpp
@@ -0,0 +1,24 @@
+#include "../include/merge_sort.hpp"
+
+int main (){
+
+    vector <int> list {78, 16, 47, 5, 54, 0, 99, 54, 25, 63, 2, 30, 6};
+
+    //  Evaluates the length of the list
+    size_t tam = list.size();
+
+    //  Prints the unsorted list
+    for (size_t i = 0; i < tam; i++){
+        cout << list.at(i) << " ";
+    }
+    cout << endl;
+
+    merge_sort(list, 0, list.size()-1);
+
+    //  Prints the sorted list
+    for (size_t i = 0; i < tam; i++){
+        cout << list.at(i) << " ";
+    }
+    cout << endl;
+
+}
